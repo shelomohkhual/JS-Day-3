ecae3cfa6b9966c53ed0fe18c6af3361012dc8bd
Shelo <siankhual96@gmail.com>
2019-07-23 10:47:08 +0800

    done ex3

--- D3_exercises/03-AJAX-Autocomplete/lib/index.js
+++ D3_exercises/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,39 @@
 // TODO: Autocomplete the input with AJAX calls.
+var autoCompleteList = document.getElementById("results");
+var searchBar = document.getElementById("search");
+
+searchBar.addEventListener("change",getInput);
+searchBar.addEventListener("keyup",getInput);
+function getInput(){
+    document.querySelectorAll("#results li").forEach((li) => li.parentNode.removeChild(li));
+    var ourRequest = new XMLHttpRequest();
+    var inputTest = document.querySelector("input[name=\"search\"]").value;
+    ourRequest.open("GET",`https://recode-dictionary.herokuapp.com/autocomplete/${inputTest}.json`);
+    // debugger;
+    ourRequest.onload = function() {
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+    };
+    ourRequest.send();
+  });		
+
+function renderHTML(data){
+  var htmlString = "";
+  var words = data.words;
+     
+  words.forEach((word)=>{
+    var liElement = document.createElement("LI");
+    var texts = document.createTextNode(`${word}`);
+    liElement.appendChild(texts);
+    autoCompleteList.appendChild(liElement);
+  });
+
+  // ** Frist Iteration ** 
+  // words.forEach((x)=>{
+  // htmlString += "<li>"+x+"</li>";
+  
+  autoCompleteList.insertAdjacentHTML("beforeend", htmlString);
+}
+
+
+
